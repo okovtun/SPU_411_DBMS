c6e1f544b1eed2e53fdd6542f2e046053ae56f82
User <User@DESKTOP-QHG18FL>
2026-01-10 13:38:29 +0300

    SELECT

---
+++ SQL/DDL/SQLQuery0-DROP DATABASE.sql
@@ -0,0 +1,4 @@
+﻿--SQLQuery0-DROP DATABASE.sql
+--https://stackoverflow.com/questions/7469130/cannot-drop-database-because-it-is-currently-in-use
+ALTER DATABASE SPU_411_DDL SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+DROP DATABASE SPU_411_DDL;
